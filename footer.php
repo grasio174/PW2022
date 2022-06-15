ed78664ab8d34953d9c4807d885a11d4d006af42
Grasio Carel <100204669+grasio174@users.noreply.github.com>
2022-06-15 11:49:21 +0800

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,27 @@
+<footer class="footer">
+
+   <section class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="#beranda"> <i class="fas fa-angle-right"></i> beranda</a>
+
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+            <a href="about.html"> <i class="fas fa-arrow-right"></i> About Us </a>
+            <a href="https://api.whatsapp.com/send?phone=6285256083785" class="links"> <i class="fas fa-phone"></i> +628-525-608-3785 </a>
+            <a href="mailto:grasiocarel074@gmail.com " class="links"> <i class="fas fa-envelope"></i> Grasiocarel074@gmail.com </a>
+            <a href="https://goo.gl/maps/XpsZdG5b5z218d3B6" class="links"> <i class="fas fa-map-marker-alt"></i>kulu, kec. wori, Minahasa Utara </a>
+      </div>
+
+      <div class="box">
+         <h3>follow kami</h3>
+         <a href="https://web.facebook.com/Enzcarel"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="https://twitter.com/Grasio21"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="https://www.instagram.com/rizandi21"> <i class="fab fa-instagram"></i> instagram </a>
+      </div>
+
+   </section>
+</footer>
